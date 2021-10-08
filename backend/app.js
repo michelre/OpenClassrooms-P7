4ee1e9e34e964bc580630d9695d6ca584b456be4
Graff Médéric <graffmederic@gmail.com>
2021-10-08 19:52:15 +0200

    Back : Ajout des packages bCrypt, body-parser, dotenv, express-rate-limit, helmet, jswonwebtoken, multer, password-validator

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,17 @@ const express = require('express');
 
 const app = express();
 
+// Ajout des headers permettant le Cross Origin Resource Sharing (CORS)
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Accès autorisé pour tous
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Accès autorisé sous certains en-têtes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Accès autorisé sous certaines méthodes
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
+// Traitement des données via bodyParser rendant celles-ci exploitables
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.json({ message : 'Votre requête a bien été reçue '});
-    next();
-});
 
-app.use ((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
