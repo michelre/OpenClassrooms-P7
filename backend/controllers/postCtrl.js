847347deea7687fa11f3c983af00cc23508fc771
Graff Médéric <graffmederic@gmail.com>
2021-10-21 17:23:44 +0200

    Back : postCtrl, fonctions deletePost, getOnePost et getAllPosts fonctionnelles

--- backend/controllers/postCtrl.js
+++ backend/controllers/postCtrl.js
@@ -40,15 +40,39 @@ exports.updatePost = (req, res, next) => {
 
 // Suppression d'une publication
 exports.deletePost = (req, res, next) => {
-    
-}
+    const id = req.params.id;
+    // Recherche de la publication via son id avant suppression 
+    db.query(`DELETE FROM publications WHERE id = ?`, id, (error, result) => {
+        // Si la publication n'a pas été trouvée
+        if (error) {
+            return res.status(400).json({ error: 'Publication non trouvée' });
+        }
+        // Si l'id correspond, suppresion de la publication
+        return res.status(204).end();
+    });
+};
 
 // Récupération d'une publication
 exports.getOnePost = (req, res, next) => {
+    const id = req.params.id;
+    db.query(`SELECT * FROM publications WHERE id = ?`, id, (error, result) => {
+        // Si la publication n'a pas été trouvée
+        if (error) {
+            return res.status(400).json({ error: 'Publication non trouvée' });
+        }
+        // Si l'id correspond, affichage de la publication
+        return res.status(200).json(result[0]);
+    });
+};
 
-}
-
-// Récupération de l'intégralité des publications 
+// Récupération de l'intégralité des publications  
 exports.getAllPosts = (req, res, next) => {
-
-}
+    db.query(`SELECT * FROM publications`, (error, result) => {
+        // Si les publications n'ont pas été trouvées
+        if (error) {
+            return res.status(400).json({ error: 'Publications non trouvées' });
+        }
+        // Si les publications ont été trouvées, affichage de tous les posts
+        return res.status(200).json(result);
+    });
+};
